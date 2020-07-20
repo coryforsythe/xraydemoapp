3bd25d523225c52d952a2b23f61b9b241202b2ef
Cory Forsythe <cforsyth@amazon.com>
2020-07-20 11:33:31 -0500

    update code

--- apps/citystateAPI/src/index.js
+++ apps/citystateAPI/src/index.js
@@ -2,11 +2,17 @@
 const express = require('express')
 const port = process.env.NODE_PORT || 3000
 const zipAPIKey=process.env.ZIP_ZPI_KEY || "YNT9w6CnPdUeIUUiC3eC4BdxoOP3trYYnvh90AMr1hovnEXlhtVfcN3dlhLaknFf"
+const http =require('http');
+const https =require('https');
 
 //Xray
 const AWSXRay = require('aws-xray-sdk-core');
 const xrayExpress = require('aws-xray-sdk-express');
-const https = AWSXRay.captureHTTPs(require('https'));
+
+AWSXRay.captureHTTPsGlobal(http);
+AWSXRay.captureHTTPsGlobal(https);
+
+const axios = require('axios');
 AWSXRay.enableAutomaticMode();
 
 //Setup ExpressJS Server
@@ -18,9 +24,9 @@ app.get('/',(req,res)=>res.status(200).send("200/OK"));
 app.get('/:zipCode', (req, res) =>{
     var segment = AWSXRay.getSegment();
     let url= "https://www.zipcodeapi.com/rest/"+zipAPIKey+"/info.json/" + req.params.zipCode + "/radians";
-    https.get(url).then((zipRes)=>{
+    axios.get(url).then((zipRes)=>{
         if(zipRes.error_msg){
-            segment.a
+            segment.addMetadata('error', zipRes.error_msg,"ResponseData");
             res.status(400).json(zipRes)
         }   
         else{
@@ -28,6 +34,9 @@ app.get('/:zipCode', (req, res) =>{
             segment.addAnnotation('state', zipRes.data.state,"ResponseData");
             res.status(200).json(zipRes.data)
         }
+    }).catch((err)=>{
+        segment.addMetadata('error', err,"ResponseData");
+        res.status(400).json(zipRes)
     });
 })
 
